e3bebf1bae72705ced777b146778f2c929f9d2be
Obsidiman01 <obsidiman01@gmail.com>
2025-01-30 10:57:54 -0800

    Changes to motor test script (No longer used)

--- Arduino/Platform/TestBench/MotorTestScript/MotorTestScript.ino
+++ Arduino/Platform/TestBench/MotorTestScript/MotorTestScript.ino
@@ -11,14 +11,19 @@ int input_control;              //Current value coming from input potentiometer
 
 //Run once on board startup
 void setup() {
+    pinMode(50, INPUT);
     set_ESC_pin(test_ESC, 52, ESC_MIN, ESC_MAX); //Default ESC pin set to pin 52
     Serial.begin(9600);
 }
 
 //Run continuously after setup completion
 void loop() {
-    int test_value = analogRead(A9);
-    Serial.println(test_value);
+    input_control = analogRead(A15);
+    
+    int target_value = map(input_control, 0, 1023, ESC_MIN, ESC_MAX);
+    
+    test_ESC.write(target_value);
+    Serial.write(digitalRead(50));
 }
 
 //Wrapper for servo.attach() to avoid overwriting existing attachment
