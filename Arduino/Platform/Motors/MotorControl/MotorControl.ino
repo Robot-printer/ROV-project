fdde4db87d8a51bdf51c01193256ede60de787fc
Obsidiman01 <Obsidiman01@gmail.com>
2025-02-13 11:57:27 -0800

    Created file for controlling motors

---
+++ Arduino/Platform/Motors/MotorControl/MotorControl.ino
@@ -0,0 +1,10 @@
+//This script controls the motors on the UUV
+#include <Servo.h>
+
+void setup(){ //Runs one time at startup
+
+}
+
+void loop(){ //Runs over and over indefinitely after startup finishes
+
+}
