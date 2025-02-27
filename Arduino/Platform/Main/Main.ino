4f3a04674ff6abda2acd621be6ccda7a9f08bcbf
Obsidiman01 <Obsidiman01@gmail.com>
2025-02-27 14:18:58 -0800

    Created Main Arduino program

---
+++ Arduino/Platform/Main/Main.ino
@@ -0,0 +1,26 @@
+//This is the main program for the Arduino in the UUV. Every other program needs to be included here so that their functions can be called.
+
+//List of programs to include in this program when it's compiled
+//Note that each #include line is replaced with the ENTIRE content of the specified file before the program is compiled
+//For now, files will be included using relative pathing, so the directory structure matters. DON'T MESS WITH THE DIRECTORY STRUCTURE OR THINGS WON'T WORK!!!
+//#include "../Motors/MotorControl/MotorControl.ino"
+//#include "../Sensors/6AxisSensor/6AxisSensor.ino"
+#include "test.cpp"
+
+//This function runs exactly once, when the Arduino is first powered up. This is where initialization, configuration, etc. should be added.
+void setup()
+{
+  //Start the Serial connection so that the Arduino and Raspberry Pi can communicate with each other
+  Serial.begin(115200);
+  //Wait for the Serial connection to open before doing anything else
+  while(!Serial)
+  {
+    delay(10); //10ms
+  }
+}
+
+//This function is called repeatedly for as long as the Arduino has power. Anything that needs to happen constantly/continuously should be here.
+void loop()
+{
+  
+}
