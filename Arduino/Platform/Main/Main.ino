036f4be72a41b3adbcaa9d456ca45c461d22fec6
Obsidiman01 <Obsidiman01@gmail.com>
2025-02-27 18:13:44 -0800

    Updated main file, mostly test code

--- Arduino/Platform/Main/Main.ino
+++ Arduino/Platform/Main/Main.ino
@@ -6,21 +6,39 @@
 #include "MotorControl.h"
 #include "6AxisSensor.h"
 
+#define test_pin 52
+Servo test_servo;
 
 //This function runs exactly once, when the Arduino is first powered up. This is where initialization, configuration, etc. should be added.
 void setup()
 {
-  //Start the Serial connection so that the Arduino and Raspberry Pi can communicate with each other
-  Serial.begin(115200);
-  //Wait for the Serial connection to open before doing anything else
-  while(!Serial)
-  {
-    delay(10); //10ms
-  }
+  //Start the serial connection with a baud rate of 115200
+  start_serial(115200);
+
+  pinMode(test_pin, OUTPUT);
+  attach_servo(test_servo, test_pin);
+
+  test_setup();
+  
 }
 
 //This function is called repeatedly for as long as the Arduino has power. Anything that needs to happen constantly/continuously should be here.
 void loop()
 {
   
+  test_servo.write(90);
+  test_loop();
+  
+}
+
+//Wrapper to start the serial connection
+void start_serial(long baud_rate)
+{
+  //Start the Serial connection so that the Arduino and Raspberry Pi can communicate with each other
+  Serial.begin(baud_rate);
+  //Wait for the Serial connection to open before doing anything else
+  while(!Serial)
+  {
+    delay(10); //10ms
+  }
 }
