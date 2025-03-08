41aa59a1fddbf21bd66e96d531edcd960bf7f91e
Obsidiman01 <Obsidiman01@gmail.com>
2025-03-08 12:49:00 -0800

    Modified communication program

--- Raspberry Pi/Platform/Communication/RasPiArdSerialComm.py
+++ Raspberry Pi/Platform/Communication/RasPiArdSerialComm.py
@@ -1,15 +1,41 @@
+#!/usr/bin/env python3
+
 #Raspberry Pi side of the RasPi-Arduino communication system
 import serial #The PySerial library
+import time
+
+comport = '/dev/ttyACM0'
+baudrate = 115200
+
+ser = serial.Serial()
+
+def open_serial(comport, baudrate):
+    global ser
+    ser = serial.Serial(comport, baudrate, timeout=1.0) #Create a Serial object called ser
+    time.sleep(3)
+    ser.reset_input_buffer()
 
 #Function to read the serial data coming in over USB port passed in comport at a baud rate of baudrate
-def read_serial(comport, baudrate):
-    ser = serial.Serial(comport, baudrate, timeout=0.1) #Create a Serial object called ser
+def read_serial():
+    global ser
+    data = ser.read_until(size=8) #Read the data coming over the serial connection and store it in a variable called data
+    return data
 
-    while True: #Do forever
-        data = ser.readline().decode().strip() #Read the data coming over the serial connection and store it in a variable called data
-
-        if data: #If data exists (ie. if it's not empty)
-            print(data) #Print the contents of data to the terminal
+def write_serial(message):
+    global ser
+    ser.write(message)
 
 if __name__ == '__main__': #Temp function to initialize the program for testing & debug
-    read_serial('/dev/ttyACM0', 115200) #Read data from port /dev/ttyACM0 at a baud rate of 115200
+    open_serial(comport, baudrate)
+    message = bytearray.fromhex('00 00 AA AA AA AA AA AA')
+    while True:
+        time.sleep(64/baudrate)
+        write_serial(message)
+        while ser.in_waiting <= 0:
+            time.sleep(64/baudrate)
+        data = read_serial()
+        if data == message:
+            print(data.hex(' ', 1))
+        else:
+            print(data)
+
