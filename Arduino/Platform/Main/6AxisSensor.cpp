d4b31a4a3640a400d3b59aeba82e86653fad9450
Obsidiman01 <Obsidiman01@gmail.com>
2025-03-08 12:47:05 -0800

    Tweaked 6 axis sensor program

--- Arduino/Platform/Main/6AxisSensor.cpp
+++ Arduino/Platform/Main/6AxisSensor.cpp
@@ -5,21 +5,12 @@
 
 #include "6AxisSensor.h"
 
-// For SPI mode, we need a CS pin
-#define LSM_CS 10
-// For software-SPI mode we need SCK/MOSI/MISO pins
-#define LSM_SCK 13
-#define LSM_MISO 12
-#define LSM_MOSI 11
-
 Adafruit_LSM6DSO32 dso32;
 void test_setup(void) {
 
   Serial.println("Adafruit LSM6DSO32 test!");
 
   if (!dso32.begin_I2C()) {
-    // if (!dso32.begin_SPI(LSM_CS)) {
-    // if (!dso32.begin_SPI(LSM_CS, LSM_SCK, LSM_MISO, LSM_MOSI)) {
     Serial.println("Failed to find LSM6DSO32 chip");
     while (1) {
       delay(10);
@@ -30,13 +21,8 @@ void test_setup(void) {
 
   dso32.setAccelRange(LSM6DSO32_ACCEL_RANGE_8_G);
 
-
   // dso32.setGyroRange(LSM6DS_GYRO_RANGE_250_DPS );
-
-
   // dso32.setAccelDataRate(LSM6DS_RATE_12_5_HZ);
-
-
   // dso32.setGyroDataRate(LSM6DS_RATE_12_5_HZ);
 
 }
