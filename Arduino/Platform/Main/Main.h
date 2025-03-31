50c4bd8ecf3a0c6bea5c43d4dc7273a3ff4acabb
Obsidiman01 <Obsidiman01@gmail.com>
2025-03-30 21:01:34 -0700

    Created Main.h to organize include files and function declarations

---
+++ Arduino/Platform/Main/Main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include "MotorControl.h"
+#include "6AxisSensor.h"
+#include "Comms.h"
+
+//Parse message to figure out which function should be called
+void parse_message(uint8_t message[8]);
+
+#endif
