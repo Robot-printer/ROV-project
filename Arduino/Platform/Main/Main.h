b819b395f2ad15192cd120ef0b5fec315dce8428
Obsidiman01 <Obsidiman01@gmail.com>
2025-03-31 20:34:51 -0700

    Added some message prefixes that were previously not included, for consistency

--- Arduino/Platform/Main/Main.h
+++ Arduino/Platform/Main/Main.h
@@ -11,14 +11,20 @@
 #define ECHO_SIMPLE 0x01
 #define ECHO_MODIFY 0x02
 #define ECHO_LOGS 0x03
+#define UNKNOWN_MESSAGE 0x0F
 //Initialization category
 #define ID_QUERY 0x10
+#define DEVICE_QUERY 0x11
+#define ACTUATOR_ID 0x12
+#define SENSOR_ID 0x13
+#define SERIAL_READY 0x1F
 //Thruster control category
 #define THRUSTER_TIMEOUT 0x20
 #define THRUSTER_THROTTLE 0x21
 //Sensor category
 #define SENSOR_UPDATE_INTERVAL 0x30
 #define SENSOR_START 0x31
+#define SENSOR_DATA 0x32
 #define SENSOR_STOP 0x3F
 
 //Parse message to figure out which function should be called
