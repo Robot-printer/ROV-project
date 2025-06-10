cdb43e15358fabd830190301b3c761fc9d39cf1b
Obsidiman01 <Obsidiman01@gmail.com>
2025-06-10 14:34:37 -0700

    minor changes & cleanup, started adding beginnings of state machine

--- Arduino/Platform/Main/Main.h
+++ Arduino/Platform/Main/Main.h
@@ -6,6 +6,12 @@
 #include "Comms.h"
 #include "MessagePrefixes.h"
 
+//States/modes for the Arduino to be in
+#define STBY 0x0 // "Standby". Do nothing, wait for instructions
+#define DBUG 0x1 // "Debug". Use functions from debug program, including echo, etc.
+#define INIT 0x2 // "Initialize". 
+
+
 //Parse message to figure out which function should be called
 void parse_message(uint8_t message[8]);
 
